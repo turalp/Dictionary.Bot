2561da2a69a53c9130cfec2ce811fc636f800440
Tural <tural.mamedov@outlook.com>
2019-09-21 12:06:39 +0300

    Added Bot, Parser, Domain and Tests projects

---
+++ Dictionary.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dictionary.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
