de517fc13064c4bf3f9b834311d479e2af9ed002
Tural <tural.mamedov@outlook.com>
2019-10-12 11:56:49 +0300

    gitflow-feature-stash: TelegramBot
    Added BotService

---
+++ Dictionary.Services/Services/BotService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Dictionary.Services.Services
+{
+    public class BotService
+    {
+        public static TelegramBotClient GetBot()
+        {
+            string token = ConfigurationManager.ConnectionStrings["BotToken"].ConnectionString;
+            var bot = new TelegramBotClient(token);
+            
+            return bot;
+        }
+
+        public static async Task Send(TelegramBotClient bot, long chatId, string message)
+        {
+            try
+            {
+                await bot.SendTextMessageAsync(
+                    chatId, 
+                    message, 
+                    ParseMode.Html, 
+                    replyMarkup: new ReplyKeyboardRemove());
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Failed sending message with message: {exception.Message}");
+            }
+        }
+    }
+}
